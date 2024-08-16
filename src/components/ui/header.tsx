0cc6efd6f0a0312765b9c6294a374018b9f49f5b
Helcio <helcioanicio19@gmail.com>
2024-08-16 09:31:59 -0300

    Feat: Added Shadcn's Carousel and update Sheet component on header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import LogoMarvel from "../../assets/logo.png";
 import {
   HomeIcon,
   Menu,
@@ -11,7 +10,6 @@ import {
   SmartphoneIcon,
   GithubIcon,
   InstagramIcon,
-  icons,
 } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "./button";
@@ -27,80 +25,91 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <Card className="shadow-sm shadow-muted">
-      <CardContent className="flex items-center justify-between p-5">
-        <h1 className="text-xl">Marvel</h1>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <Menu className="text-3xl text-white" size={16} />
-            </Button>
-          </SheetTrigger>
+    <>
+      <Card className="shadow-sm shadow-muted">
+        <CardContent className="flex items-center justify-between px-5 py-0">
+          <figure>
+            <Image
+              src="/logo.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              alt="Logo Marvel"
+              className="w-24"
+            ></Image>
+          </figure>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Menu className="text-3xl text-white" size={16} />
+              </Button>
+            </SheetTrigger>
 
-          <SheetContent className="flex h-full flex-col gap-8">
-            <SheetHeader className="text-lg font-semibold">Menu</SheetHeader>
-            <SheetClose asChild>
-              <Link href="/">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <HomeIcon size={16} />
-                  Home
-                </Button>
-              </Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link href="/">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <VenetianMaskIcon size={16} />
-                  Characters
-                </Button>
-              </Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link href="/">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <BookUserIcon size={16} />
-                  Comics
-                </Button>
-              </Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link href="/">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  Contact
-                </Button>
-              </Link>
-            </SheetClose>
-
-            <SheetFooter className="mt-auto w-full">
-              <SheetClose className="flex gap-3">
-                <Link href="https://linkedin.com/in/helcio-anicio">
-                  <Button>
-                    <LinkedinIcon />
-                  </Button>
-                </Link>
-                <Link href="https://linkedin.com/in/helcio-anicio">
-                  <Button>
-                    <SmartphoneIcon />
+            <SheetContent className="flex h-full flex-col gap-8">
+              <SheetHeader className="text-lg font-semibold">Menu</SheetHeader>
+              <SheetClose asChild>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <HomeIcon size={16} />
+                    Home
                   </Button>
                 </Link>
               </SheetClose>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
+              <SheetClose asChild>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <VenetianMaskIcon size={16} />
+                    Characters
+                  </Button>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <BookUserIcon size={16} />
+                    Comics
+                  </Button>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    Contact
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetFooter className="mt-auto w-full">
+                <SheetClose className="flex gap-3">
+                  <Link href="https://linkedin.com/in/helcio-anicio">
+                    <Button>
+                      <LinkedinIcon />
+                    </Button>
+                  </Link>
+                  <Link href="https://linkedin.com/in/helcio-anicio">
+                    <Button>
+                      <SmartphoneIcon />
+                    </Button>
+                  </Link>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
